7fc9f25b1ffcc09d433ad3f8999205b89fb9d86e
aKittyCat <holotaro@gmail.com>
2025-08-26 21:44:25 +0700

    gem

---
+++ gem-allowed.js
@@ -0,0 +1,17 @@
+window.GEM_RULES = {
+  10:   1.00,
+  50:   1.10,
+  100:  1.25,
+  500:  1.50,
+  1000: 1.75,
+  5000: 2.00,
+};
+
+window.GEM_GROUPS = {
+  10:   ["Azurite","Banded Agate","Blue Quartz","Eye Agate","Hematite","Lapis Lazuli","Malachite","Moss Agate","Obsidian","Rhodochrosite","Tiger Eye","Turquoise"],
+  50:   ["Bloodstone","Carnelian","Chalcedony","Chrysoprase","Citrine","Jasper","Moonstone","Onyx","Rock Crystal","Sard","Sardonyx","Zircon"],
+  100:  ["Amber","Amethyst","Chrysoberyl","Coral","Garnet","Jade","Jet","Pearl","Spinel","Tourmaline"],
+  500:  ["Alexandrite","Aquamarine","Black Pearl","Blue Spinel","Peridot","Topaz"],
+  1000: ["Black Opal","Blue Sapphire","Emerald","Fire Opal","Opal","Star Ruby","Star Sapphire","Yellow Sapphire"],
+  5000: ["Black Sapphire","Diamond","Jacinth","Ruby"]
+};
