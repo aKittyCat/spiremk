fb9794b197da2b9dfd2677c736342e971b2d5099
aKittyCat <holotaro@gmail.com>
2025-08-28 20:23:53 +0700

    gv-allowed-update

--- gv-allowed.js
+++ gv-allowed.js
@@ -16,9 +16,42 @@
     "Twinshot Hand Crossbow","War Pick","Warhammer","Whip","Yklwa"
   ];
 
+  const BLOODEDSHED_BLADE_BASES = [
+    "Double-Bladed Scimitar","Greatsword","Longsword","Rapier","Scimitar","Shortsword"
+  ];
+
+  const GLIMMERLING_MOONBOW_BASES = [
+    "Longbow","Shortbow"
+  ];
+
+  const STARSHOT_CROSSBOW_BASES = [
+    "Hand Crossbow","Heavy Crossbow","Light Crossbow","Light Repeating Crossbow"
+  ];
+
   const ARMOR_BASES = [
     "Breastplate","Chain Mail","Chain Shirt","Half Plate Armor","Hide Armor","Leather Armor","Padded Armor","Plate Armor",
     "Ring Mail","Scale Mail", "Spiked Armor","Splint Armor","Studded Leather Armor"
+  ];
+
+  const ARMOR_OF_FUNGAL_SPORES_BASES = [ 
+    "Breastplate","Chain Shirt","Half Plate Armor","Hide Armor","Scale Mail","Spiked Armor"
+  ];
+
+  const ARMOR_OF_THE_FALLEN_BASES = [ 
+    "Breastplate","Chain Mail","Chain Shirt","Half Plate Armor","Hide Armor","Plate Armor","Ring Mail","Scale Mail","Spiked Armor","Splint Armor"
+  ];
+
+  const ARMOR_OF_SAFEGUARDING_BASES = [ 
+    "Chain Mail","Plate Armor","Ring Mail","Splint Armor"
+  ];
+
+  const MITHRAL_ARMOR_BASES = [ 
+    "Breastplate","Chain Mail","Chain Shirt","Half Plate Armor","Plate Armor",
+    "Ring Mail","Scale Mail", "Spiked Armor","Splint Armor"
+  ];
+
+  const ZEPHYR_ARMOR_BASES = [ 
+    "Leather Armor","Padded Armor","Studded Leather Armor"
   ];
 
   const SHIELD_BASES = [
@@ -29,26 +62,38 @@
 
   // ตั้งค่าวิธีสร้างชื่อ "ผลลัพธ์หลังอัพเกรด" ต่อ base item แต่ละ variant
   const GV_ALLOWED = {
-    "weapon+1":           { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `${base} +1 ` },
-    "weapon+2":           { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `${base} +2 ` },
-    "weapon+3":           { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `${base} +3 ` },
-    "armor+1":            { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} +1 ` },
-    "armor+2":            { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} +2 ` },
-    "armor+3":            { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} +3 ` },
-    "shield+1":           { kind: "shield",   list: makeSet(SHIELD_BASES),            makeName: (base)=> `${base} +1 ` },
-    "shield+2":           { kind: "shield",   list: makeSet(SHIELD_BASES),            makeName: (base)=> `${base} +2 ` },
-    "shield+3":           { kind: "shield",   list: makeSet(SHIELD_BASES),            makeName: (base)=> `${base} +3 ` },
+    "weapon+1":           { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `${base} +1` },
+    "weapon+2":           { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `${base} +2` },
+    "weapon+3":           { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `${base} +3` },
+    "armor+1":            { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} +1` },
+    "armor+2":            { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} +2` },
+    "armor+3":            { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} +3` },
+    "shield+1":           { kind: "shield",   list: makeSet(SHIELD_BASES),            makeName: (base)=> `${base} +1` },
+    "shield+2":           { kind: "shield",   list: makeSet(SHIELD_BASES),            makeName: (base)=> `${base} +2` },
+    "shield+3":           { kind: "shield",   list: makeSet(SHIELD_BASES),            makeName: (base)=> `${base} +3` },
     "adamantine-weapon":  { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Adamantine ${base}` },
     "adamantine-armor":   { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `Adamantine ${base}` },
+    "armor-of-fungal-spores": { kind: "armor",   list: makeSet(ARMOR_OF_FUNGAL_SPORES_BASES), makeName: (base)=> `${base} Armor of Fungal Spores` },
+    "armor-of-the-fallen": { kind: "armor",   list: makeSet(ARMOR_OF_THE_FALLEN_BASES), makeName: (base)=> `${base} Armor of the Fallen` },
+    "armor-of-safeguarding": { kind: "armor",   list: makeSet(ARMOR_OF_SAFEGUARDING_BASES), makeName: (base)=> `${base} Armor of Safeguarding` },
+    "armor-of-weightlessness": { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `${base} Armor of Weightlessness` },
+    "bloodshed-blade":   { kind: "weapon",  list: makeSet(BLOODEDSHED_BLADE_BASES), makeName: (base)=> `Bloodshed ${base}` },
+    "bow-of-conflagration": { kind: "weapon",  list: makeSet(GLIMMERLING_MOONBOW_BASES), makeName: (base)=> `${base} of Conflagration` },
+    "fool's-blade":     { kind: "weapon",  list: makeSet(BLOODEDSHED_BLADE_BASES),          makeName: (base)=> `Fool's ${base}` },
+    "glimmerling-moonbow": { kind: "weapon",  list: makeSet(GLIMMERLING_MOONBOW_BASES), makeName: (base)=> `Glimmerling Moon ${base}` },
+    "hellfire-weapon":  { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Hellfire ${base}` },
+    "mithral-armor":    { kind: "armor",   list: makeSet(MITHRAL_ARMOR_BASES),    makeName: (base)=> `Mithral ${base}` },
     "oceanic-weapon":   { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Oceanic ${base}` },
     "ruidium-weapon":   { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Ruidium ${base}` },
     "smoldering-armor": { kind: "armor",   list: makeSet(ARMOR_BASES),           makeName: (base)=> `Smoldering ${base}` },
+    "starshot-crossbow": { kind: "weapon",  list: makeSet(STARSHOT_CROSSBOW_BASES), makeName: (base)=> `Starshot ${base}` },
     "silvered-weapon":   { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Silvered ${base}` },
     "vicious-weapon":    { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Vicious ${base}` },
-    "vicious-weapon+1": { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Vicious ${base} +1 ` },
+    "vicious-weapon+1": { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Vicious ${base} +1` },
     "weapon-of-certain-death": { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Weapon of Certain Death ${base}` },
     "weapon-of-throne's-command": { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Weapon of Throne's Command ${base}` },
     "weapon-of-warning": { kind: "weapon",  list: makeSet(WEAPON_BASES),          makeName: (base)=> `Weapon of Warning ${base}` },
+    "zephyr-armor":     { kind: "armor",   list: makeSet(ZEPHYR_ARMOR_BASES),     makeName: (base)=> `Zephyr ${base}` },
     // เพิ่มคีย์ใหม่ ๆ ได้ตามต้องการ เช่น:
     // "armor+2": { kind: "armor", list: makeSet([...]), makeName: (base)=> `+2 ${base}` },
   };
